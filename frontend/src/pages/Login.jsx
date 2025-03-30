e664ecf001eaccc31c97bc74d1aee73e82589dee
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-03-30 12:29:27 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -180,7 +180,7 @@ const Login = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = 'https://zupewh60yi.execute-api.ap-south-1.amazonaws.com/default/learnandshare-api/api/auth/google';
+    window.location.href = 'https://zupewh60yi.execute-api.ap-south-1.amazonaws.com/prod/api/auth/google';
 
   };
 
